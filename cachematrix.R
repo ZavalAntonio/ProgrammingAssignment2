d78e602ede0b00fcecb02f2258be7567c42cde68
ZavalAntonio <zavalantonio96@gmail.com>
2017-03-12 19:45:42 -0600

    initial comment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#This function get the inverse of a matrix 
 
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        inverse.matrix <- function(solve) m <<- solve
+        get.inverse.matrix <- function() m
+        list(set = set, get = get, inverse.matrix = inverse.matrix,
+             get.inverse.matrix = get.inverse.matrix)
 }
 
 
 ## Write a short comment describing this function
+#This function gets the result of a Cached Matrix if it was not solved by the last function.
 
 cacheSolve <- function(x, ...) {
+        m <- x$get.inverse.matrix()
+        if(!is.null(m)){
+                message("getting chaded data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$inverse.matrix(m)
+        m
         ## Return a matrix that is the inverse of 'x'
 }
